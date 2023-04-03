4e679bd253d74a08e6c6cfef539229aa332992dd
James Yoo <jyoo3607@gmail.com>
2023-04-03 15:42:21 -0700

    update

--- Parameter.java
+++
@@ -1,9 +0,0 @@
-/**
- * Parameters that the user can control in the application.
- * 
- * @author Macguire Rintoul
- * @author https://macguire.me
- */
-enum Parameter {
-	radius, length, angle
-}
--- RangeSliderWindow.java
+++ RangeSliderWindow.java
@@ -12,31 +12,32 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
+import javax.swing.border.Border;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 public class RangeSliderWindow extends JPanel implements ActionListener, ChangeListener {
 	private static final long serialVersionUID = 1L;
 	private FinalProject finalProject; // reference to the finalProject
-	private RangeSlider radiusSlider = new RangeSlider(); // double-input slider to control stroke radius
-	private RangeSlider lengthSlider = new RangeSlider(); // double-input slider to control stroke length
+	private RangeSlider radius_slider = new RangeSlider(); // double-input slider to control stroke radius
+	private RangeSlider length_slider = new RangeSlider(); // double-input slider to control stroke length
 
-	private JSlider pixelIntervalSlider; // single-input slider to control pixel interval
+	private JSlider pixel_gap_slider; // single-input slider to control pixel interval
 
 	// labels for sliders and values
-	private JLabel radiusSliderTitle = new JLabel();
-	private JLabel radiusSliderLabel1 = new JLabel();
-	private JLabel radiusSliderValue1 = new JLabel();
-	private JLabel radiusSliderLabel2 = new JLabel();
-	private JLabel radiusSliderValue2 = new JLabel();
+	private JLabel radius_slider_title = new JLabel();
+	private JLabel radius_slider_label_min = new JLabel();
+	private JLabel radius_slider_value_min = new JLabel();
+	private JLabel radius_slider_label_max = new JLabel();
+	private JLabel range_slider_value_max = new JLabel();
 
-	private JLabel lengthSliderTitle = new JLabel();
-	private JLabel lengthSliderLabel1 = new JLabel();
-	private JLabel lengthSliderValue1 = new JLabel();
-	private JLabel lengthSliderLabel2 = new JLabel();
-	private JLabel lengthSliderValue2 = new JLabel();
+	private JLabel length_slider_title = new JLabel();
+	private JLabel length_slider_label_min = new JLabel();
+	private JLabel length_slider_value_min = new JLabel();
+	private JLabel length_slider_label_max = new JLabel();
+	private JLabel length_slider_value_max = new JLabel();
 
-	private JLabel pixelIntervalSliderTitle = new JLabel();
+	private JLabel pixel_gap_sliderTitle = new JLabel();
 	private JButton applyBtn = new JButton("Apply"); // apply the current parameters
 
 	/**
@@ -45,120 +46,99 @@ public class RangeSliderWindow extends JPanel implements ActionListener, ChangeL
 	 * @param finalProject the finalProject instance
 	 */
 	public RangeSliderWindow(FinalProject finalProject) {
-		setBorder(BorderFactory.createEmptyBorder(6, 6, 6, 6));
+		Border empty = BorderFactory.createRaisedBevelBorder();
+
+		setBorder(empty);
 		setLayout(new GridBagLayout());
 		this.finalProject = finalProject;
 
-//		JSlider(orientation,mixValue, maxValue, value);
-		pixelIntervalSlider = new JSlider(JSlider.HORIZONTAL, finalProject.minAllowedPixelInterval,
-				finalProject.maxAllowedPixelInterval, finalProject.pixelInterval);
+		// JSlider(orientation,mixValue, maxValue, value);
+		pixel_gap_slider = new JSlider(JSlider.HORIZONTAL, finalProject.min_slider_interval,
+				finalProject.max_slider_interval, finalProject.pixel_gap);
 
-		// add change listeners to sliders
-		pixelIntervalSlider.addChangeListener(this);
-		lengthSlider.addChangeListener(this);
-		radiusSlider.addChangeListener(this);
+		//
+		pixel_gap_slider.addChangeListener(this);
+		length_slider.addChangeListener(this);
+		radius_slider.addChangeListener(this);
 
-		// create pixel interval slider
-		pixelIntervalSliderTitle.setText("Pixel interval");
-		pixelIntervalSlider.setPreferredSize(new Dimension(240, 40));
-		pixelIntervalSlider.setMajorTickSpacing(1);
-		pixelIntervalSlider.setPaintTicks(true);
-		pixelIntervalSlider.setPaintLabels(true);
-		pixelIntervalSlider.setSnapToTicks(true);
+		// pixel gap
+		/*
+		 * keep this??
+		 */
+		pixel_gap_sliderTitle.setText("Pixel Gap/Increment");
+		pixel_gap_slider.setPreferredSize(new Dimension(240, 40));
+		pixel_gap_slider.setMajorTickSpacing(1);
+		pixel_gap_slider.setPaintTicks(true);
+		pixel_gap_slider.setPaintLabels(true);
+		pixel_gap_slider.setSnapToTicks(true);
 
-		// create radius slider
+		// radius slider
 
-		radiusSlider.setPreferredSize(new Dimension(240, radiusSlider.getPreferredSize().height));
-		radiusSlider.setMinimum(finalProject.minAllowedStrokeRadius);
-		radiusSlider.setMaximum(finalProject.maxAllowedStrokeRadius);
-		radiusSlider.setValue(finalProject.minStrokeRadius);
-		radiusSlider.setUpperValue(finalProject.maxStrokeRadius);
-		radiusSliderTitle.setText("Stroke radius");
-		radiusSliderLabel1.setText("Lower value:");
-		radiusSliderLabel2.setText("Upper value:");
+		radius_slider.setPreferredSize(new Dimension(240, radius_slider.getPreferredSize().height));
+		radius_slider.setMinimum(finalProject.min_slider_radius);
+		radius_slider.setMaximum(finalProject.max_slider_radius);
+		radius_slider.setValue(finalProject.min_slider_radius);
+		radius_slider.setUpperValue(finalProject.max_slider_radius);
+		radius_slider_title.setText("Stroke radius");
+		radius_slider_label_min.setText("Min value:");
+		radius_slider_label_max.setText("Max value:");
 
-		// create length slider
-
-		lengthSlider.setPreferredSize(new Dimension(240, lengthSlider.getPreferredSize().height));
-		lengthSlider.setMinimum(finalProject.minAllowedStrokeLength);
-		lengthSlider.setMaximum(finalProject.maxAllowedStrokeLength);
-		lengthSlider.setValue(finalProject.minStrokeLength);
-		lengthSlider.setUpperValue(finalProject.maxStrokeLength);
-		lengthSliderTitle.setText("Stroke length");
-		lengthSliderLabel1.setText("Lower value:");
-		lengthSliderLabel2.setText("Upper value:");
+		// length slider
+		length_slider.setPreferredSize(new Dimension(240, length_slider.getPreferredSize().height));
+		length_slider.setMinimum(finalProject.min_slider_length);
+		length_slider.setMaximum(finalProject.max_slider_length);
+		length_slider.setValue(finalProject.min_slider_length);
+		length_slider.setUpperValue(finalProject.max_slider_length);
+		length_slider_title.setText("Stroke length");
+		length_slider_label_min.setText("Min value:");
+		length_slider_label_max.setText("Max value:");
 
 		// Add action listeners to buttons
 		applyBtn.addActionListener(this);
 
-		Insets noInsets = new Insets(0, 0, 0, 0);
-		Insets marginTop = new Insets(16, 0, 4, 0);
+		add(pixel_gap_sliderTitle, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+				GridBagConstraints.NONE, new Insets(0, 0, 3, 3), 0, 0));
 
-		/*
-		 * PIXEL INTERVAL SLIDER
-		 */
+		add(pixel_gap_slider, new GridBagConstraints(0, 1, 2, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+				GridBagConstraints.NONE, new Insets(0, 0, 3, 3), 0, 0));
 
-		// Row 0
-		add(pixelIntervalSliderTitle, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
-				GridBagConstraints.NONE, marginTop, 0, 0));
+		add(radius_slider_title, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+				GridBagConstraints.NONE, new Insets(0, 0, 3, 3), 0, 0));
 
-		// Row 1
-		add(pixelIntervalSlider, new GridBagConstraints(0, 1, 2, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
-				GridBagConstraints.NONE, noInsets, 0, 0));
+		add(radius_slider_label_min, new GridBagConstraints(0, 3, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+				GridBagConstraints.NONE, new Insets(0, 0, 3, 3), 0, 0));
+		add(radius_slider_value_min, new GridBagConstraints(1, 3, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+				GridBagConstraints.NONE, new Insets(0, 0, 3, 3), 0, 0));
 
-		/*
-		 * RADIUS SLIDER
-		 */
+		add(radius_slider_label_max, new GridBagConstraints(0, 4, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+				GridBagConstraints.NONE, new Insets(0, 0, 3, 3), 0, 0));
+		add(range_slider_value_max, new GridBagConstraints(1, 4, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+				GridBagConstraints.NONE, new Insets(0, 0, 3, 3), 0, 0));
 
-		// Row 2
-		add(radiusSliderTitle, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
-				GridBagConstraints.NONE, marginTop, 0, 0));
+		add(radius_slider, new GridBagConstraints(0, 5, 2, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+				GridBagConstraints.NONE, new Insets(0, 0, 3, 3), 0, 0));
 
-		// Row 3
-		add(radiusSliderLabel1, new GridBagConstraints(0, 3, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
-				GridBagConstraints.NONE, noInsets, 0, 0));
-		add(radiusSliderValue1, new GridBagConstraints(1, 3, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
-				GridBagConstraints.NONE, noInsets, 0, 0));
+		add(length_slider_title, new GridBagConstraints(0, 6, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+				GridBagConstraints.NONE, new Insets(0, 0, 3, 3), 0, 0));
 
-		// Row 4
-		add(radiusSliderLabel2, new GridBagConstraints(0, 4, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
-				GridBagConstraints.NONE, noInsets, 0, 0));
-		add(radiusSliderValue2, new GridBagConstraints(1, 4, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
-				GridBagConstraints.NONE, noInsets, 0, 0));
+		add(length_slider_label_min, new GridBagConstraints(0, 7, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+				GridBagConstraints.NONE, new Insets(0, 0, 3, 3), 0, 0));
+		add(length_slider_value_min, new GridBagConstraints(1, 7, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+				GridBagConstraints.NONE, new Insets(0, 0, 3, 3), 0, 0));
 
-		// Row 5
-		add(radiusSlider, new GridBagConstraints(0, 5, 2, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
-				GridBagConstraints.NONE, noInsets, 0, 0));
+		add(length_slider_label_max, new GridBagConstraints(0, 8, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+				GridBagConstraints.NONE, new Insets(0, 0, 3, 3), 0, 0));
+		add(length_slider_value_max, new GridBagConstraints(1, 8, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+				GridBagConstraints.NONE, new Insets(0, 0, 3, 3), 0, 0));
 
-		/*
-		 * LENGTH SLIDER
-		 */
-
-		// Row 6
-		add(lengthSliderTitle, new GridBagConstraints(0, 6, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
-				GridBagConstraints.NONE, marginTop, 0, 0));
-
-		// Row 7
-		add(lengthSliderLabel1, new GridBagConstraints(0, 7, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
-				GridBagConstraints.NONE, noInsets, 0, 0));
-		add(lengthSliderValue1, new GridBagConstraints(1, 7, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
-				GridBagConstraints.NONE, noInsets, 0, 0));
-
-		// Row 8
-		add(lengthSliderLabel2, new GridBagConstraints(0, 8, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
-				GridBagConstraints.NONE, noInsets, 0, 0));
-		add(lengthSliderValue2, new GridBagConstraints(1, 8, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
-				GridBagConstraints.NONE, noInsets, 0, 0));
-
-		// Row 9
-		add(lengthSlider, new GridBagConstraints(0, 9, 2, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
-				GridBagConstraints.NONE, noInsets, 0, 0));
+		add(length_slider, new GridBagConstraints(0, 9, 2, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+				GridBagConstraints.NONE, new Insets(0, 0, 3, 3), 0, 0));
 
 		/*
 		 * buttons
 		 */
 		add(applyBtn, new GridBagConstraints(0, 16, 2, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
-				GridBagConstraints.NONE, noInsets, 0, 0));
+				GridBagConstraints.NONE, new Insets(0, 0, 3, 3), 0, 0));
 	}
 
 	/**
@@ -167,18 +147,12 @@ public class RangeSliderWindow extends JPanel implements ActionListener, ChangeL
 	 * @param e the event associated with the change
 	 */
 	public void stateChanged(ChangeEvent e) {
-		if (e.getSource() == lengthSlider) {
-			lengthSliderValue1.setText(String.valueOf(lengthSlider.getValue()));
-			lengthSliderValue2.setText(String.valueOf(lengthSlider.getUpperValue()));
-			System.out.println("LENGTH SLIDER CHANGED");
-		} else if (e.getSource() == radiusSlider) {
-			radiusSliderValue1.setText(String.valueOf(radiusSlider.getValue()));
-			radiusSliderValue2.setText(String.valueOf(radiusSlider.getUpperValue()));
-			System.out.println("RADIUS SLIDER CHANGED");
-		} else if (e.getSource() == pixelIntervalSlider) {
-			if (!pixelIntervalSlider.getValueIsAdjusting()) {
-				System.out.println("pixel interval slider changed");
-			}
+		if (e.getSource() == length_slider) {
+			length_slider_value_min.setText(String.valueOf(length_slider.getValue()));
+			length_slider_value_max.setText(String.valueOf(length_slider.getUpperValue()));
+		} else if (e.getSource() == radius_slider) {
+			radius_slider_value_min.setText(String.valueOf(radius_slider.getValue()));
+			range_slider_value_max.setText(String.valueOf(radius_slider.getUpperValue()));
 		}
 	}
 
@@ -188,27 +162,23 @@ public class RangeSliderWindow extends JPanel implements ActionListener, ChangeL
 	 * @param event the event associated with the click
 	 */
 	public void actionPerformed(ActionEvent event) {
-		JButton clickedButton = (JButton) event.getSource();
-		if (clickedButton == applyBtn) {
-			btn();
-		}
+		cta();
 	}
 
-	/** Apply the slider values to the finalProject class. */
-	private void btn() {
-		finalProject.pixelInterval = pixelIntervalSlider.getValue();
-		finalProject.setRange(Parameter.radius, radiusSlider.getValue(), radiusSlider.getUpperValue());
-		finalProject.setRange(Parameter.length, lengthSlider.getValue(), lengthSlider.getUpperValue());
+	private void cta() {
+		finalProject.pixel_gap = pixel_gap_slider.getValue();
+		finalProject.min_max(Parameter.RADIUS, radius_slider.getValue(), radius_slider.getUpperValue());
+		finalProject.min_max(Parameter.LENGTH, length_slider.getValue(), length_slider.getUpperValue());
 	}
 
-	/** Display the UI control window. */
-	public void display() {
-		// Display the current values of the sliders
-		radiusSliderValue1.setText(String.valueOf(radiusSlider.getValue()));
-		radiusSliderValue2.setText(String.valueOf(radiusSlider.getUpperValue()));
-		lengthSliderValue1.setText(String.valueOf(lengthSlider.getValue()));
-		lengthSliderValue2.setText(String.valueOf(lengthSlider.getUpperValue()));
+	public void sliderValue() {
+		radius_slider_value_min.setText("" + radius_slider.getValue());
+		range_slider_value_max.setText("" + radius_slider.getUpperValue());
+		length_slider_value_min.setText("" + length_slider.getValue());
+		length_slider_value_max.setText("" + length_slider.getUpperValue());
+	}
 
+	public void setFrame() {
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
@@ -219,4 +189,10 @@ public class RangeSliderWindow extends JPanel implements ActionListener, ChangeL
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 	}
+
+	public void display() {
+		sliderValue();
+		setFrame();
+	}
+
 }
